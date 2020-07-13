5d38bf6cecf8d277b2da64ab51595f60d069cefe
akshay-misra-backend-products <akshaymishra2488@gmail.com>
2020-07-14 02:02:25 +0530

    gbss api gateway first commit

---
+++ src/test/java/com/gbss/api/gateway/GbssApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbss.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbssApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
